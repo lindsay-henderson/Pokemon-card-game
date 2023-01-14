d9bfae100fba47029cb929cc3e7a31600eacb9ea
LindsayH <lindsaynhenderson@gmail.com>
2023-01-13 18:41:43 -0600

    write function to find total experience in each hand of cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
 import React from 'react'
-import Pokedex from './Pokedeck/Pokedex';
-
+// import Pokedex from './Pokedeck/Pokedex';
+import Pokegame from './Pokedeck/Pokegame';
 function App() {
   return (
     <div className="App">
-      <Pokedex/>
+      <Pokegame/>
     </div>      
 
 
