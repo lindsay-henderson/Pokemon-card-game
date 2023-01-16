539b80f12ec7314d3ba08e2709e7d4301e2b699e
LindsayH <lindsaynhenderson@gmail.com>
2023-01-16 10:57:20 -0600

    style pokecard

--- src/Pokedeck/Pokedex.js
+++ src/Pokedeck/Pokedex.js
@@ -5,6 +5,12 @@ import Pokecard from './Pokecard'
 class Pokedex extends Component {
 
   render() {
+    let title
+    if (this.props.isWinner) {
+      title = <h1 className ="Pokedex-winner">Winning Hand</h1> 
+    } else {
+      <h1 className = "Pokedex-loser">Losing Hand</h1>
+    }
     return (
       <div className='Pokedex'>
         <h1>Pokedex!</h1>
