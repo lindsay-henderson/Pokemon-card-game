10b3eb66d2017849d893fb0e1ef9cd330d47abdd
LindsayH <lindsaynhenderson@gmail.com>
2023-01-16 11:51:19 -0600

    make rule for displaying winning and losing declarative text

--- src/Pokedeck/Pokedex.js
+++ src/Pokedeck/Pokedex.js
@@ -5,17 +5,17 @@ import Pokecard from './Pokecard'
 class Pokedex extends Component {
 
   render() {
-    let title
+    let title 
     if (this.props.isWinner) {
-      <h1 className ="Pokedex-winner">Winning Hand</h1> 
+      title = <h1 className ="Pokedex-winner">Winning Hand</h1> 
     } else {
-      <h1 className = "Pokedex-loser">Losing Hand</h1>
+      title = <h1 className = "Pokedex-loser">Losing Hand</h1>
     }
     return (
       <div className='Pokedex'>
         <h1>Pokedex!</h1>
         <p>Total Experience: {this.props.exp}</p>
-        <p>{this.props.isWinner ? 'WINNER' : 'LOSER'}</p>
+        {title}
         <div className='Pokedex-cards'>
         {this.props.pokemon.map((p)=> (
           <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
